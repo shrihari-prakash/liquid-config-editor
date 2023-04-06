f9062abdb873853c200a3ac04c147441841a4a73
Shri <35889246+shrihari-prakash@users.noreply.github.com>
2023-04-07 02:09:22 +0530

    Increased remount timeout while importing options.

--- src/components/BackendConfig/index.jsx
+++ src/components/BackendConfig/index.jsx
@@ -100,7 +100,7 @@ export default function BackendConfig() {
         return newOptions;
       });
       setLoading(true);
-      setTimeout(() => setLoading(false), 0);
+      setTimeout(() => setLoading(false), 100);
     });
   };
 
